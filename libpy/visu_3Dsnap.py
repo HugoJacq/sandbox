911745e97dd1c11bdfd4e3570f7fd1d165932b5f
Hugo JACQUET <hugo.jacquet1@univ-grenoble-alpes.fr>
2026-08-11 11:20:04 +0200

    better camera position and colorbar

--- libpy/visu_3Dsnap.py
+++ libpy/visu_3Dsnap.py
@@ -209,6 +209,16 @@ def render_snapshot(
     plotter.enable_3_lights()
     plotter.set_background(background)
 
+    pos = plotter.camera.position
+    shiftV = 1 / 3 * H0
+    shiftH = 1 / 5 * H0
+    pos = (pos[0] + shiftH, pos[1], pos[2] - shiftV)
+    fp = plotter.camera.focal_point
+    plotter.camera.focal_point = (fp[0] + shiftH, fp[1], fp[2] - shiftV)
+    plotter.camera.position = pos
+    plotter.camera.view_angle = 35
+    # plotter.camera.zoom =
+
     ds = build_z_ds(ds, "float32")
     grid = build_3Dgrid(ds)
 
