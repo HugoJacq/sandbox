81843319cc65e4fec1d9c70f96f9cd5c7086eb7f
Hugo JACQUET <hugo.jacquet1@univ-grenoble-alpes.fr>
2026-09-08 09:23:33 +0200

    added figures for CSI, and cleanup

--- breaking_strat/script_CSI_report_2026.py
+++ breaking_strat/script_CSI_report_2026.py
@@ -17,14 +17,14 @@ dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, "../libpy/")
 sys.path.append(filename)
 from data_reader import read_bas_data, build_grid
-from diags import compute_us_Kenyon1969
+from diags import compute_us_Kenyon1969, grad_dir
 from L2_maker import make_L2_layer, make_L2_eulerian
 from visu_3Dsnap import render_snapshot
 from fftlib import get_spec_1D, get_wavenumber
 
 # General parameters
 L0 = 200
-H0 = 100
+H0 = 20
 N = 512
 g = 9.81
 kp = 10 * np.pi / L0
@@ -35,11 +35,12 @@ Ndeux = 2e-6
 betaT = 2e-4
 Ts = 20.0
 analysis = "Lagrange"  # Euler or Lagrange
-hchunks = 256
+hchunks = 128
 
-inpath = "N512_nl30_0.000002_Tinizl/"
+# inpath = "N512_nl30_0.000002_Tinizl/"
 # inpath = "N512_30_P0.02_RE40000_TiniZ/"
-inpath = "N512_30_P0.02_RE40000_TiniZl/"
+# inpath = "N512_30_P0.02_RE40000_TiniZl/"
+inpath = "N512_40_RE40000_TiniZl_remap1.0/"
 infile = "out.nc"
 outfile = "L2_" + infile
 lsfx = -1
@@ -67,25 +68,83 @@ Zm = dsL2.z.mean(dim=["x", "y"])
 colors = cc.fire[:: len(cc.fire) // nt]
 
 
+# --- Energy evolution ---
+if False:
+    skip = 20
+
+    def compute_Ek(ds, skip=2):
+        return (
+            0.5
+            * (
+                (
+                    ds["u.x"].isel(time=slice(0, len(ds.time), skip)) ** 2
+                    + ds["u.y"].isel(time=slice(0, len(ds.time), skip)) ** 2
+                    + ds["u.z"].isel(time=slice(0, len(ds.time), skip)) ** 2
+                )
+                * dx**2
+            ).sum(dim=["level", "x", "y"])
+            / (L0 * L0)
+        )
+
+    def EOS(T, T0, betaT):
+        drho = -betaT * (T - T0)
+        return drho
+
+    def compute_Ep(rho, z, h, Ep0, skip=2):
+        Ept = (
+            (rho * g * z * h)
+            .isel(time=slice(0, len(ds.time), skip))
+            .sum(dim=["level", "x", "y"])
+        )
+        # Ep0 = Ept.sel(time=0)
+        return (Ept - Ep0) / (L0**2)
+
+    drho = EOS(ds.T, Ts, betaT)
+
+    dz0 = H0 / nl
+    z0 = np.arange(-H0 + dz0, 0, dz0)
+    Tini = Ndeux / (g * betaT) * z0 + Ts
+    drho0 = EOS(Tini, Ts, betaT)
+
+    Ep0 = np.sum((1 + drho0) * g * z0 * dz0)
+    Ep0 = 0
+    # Ep /rho0
+    Ep = compute_Ep((1 + drho), ds.z, ds.h, Ep0, skip=skip)
+    Ek = compute_Ek(ds, skip=skip)
+    Et = Ep + Ek
+    ET0 = Et[0].values
+    fig, ax = plt.subplots(1, 1, figsize=(5, 5), constrained_layout=True, dpi=100)
+    ax.plot(ds.time[::skip] / Tp, 2 * Ep / ET0, c="g", label="2Ep")
+    ax.plot(ds.time[::skip] / Tp, 2 * Ek / ET0, c="b", label="2Ek")
+    ax.plot(ds.time[::skip] / Tp, Ep + Ek / ET0, c="k", label="Et=Ek+Ep")
+    ax.set_xlabel("t/Tp")
+    ax.set_ylabel("E")
+    fig.savefig("CSI_energy_evolution.pdf")
+
 # --- mean profiles evolution plots ---
-if True:
+if False:
     skip = 10
 
     fig, ax = plt.subplots(1, 3, figsize=(10, 5), constrained_layout=True, dpi=100)
     print("-> Tm profiles")
     for it in range(0, nt, skip):
         ax[0].plot(
-            (dsL2.T_m.isel(time=it) - Ts) * 1000, Zm.isel(time=it), c="b", alpha=0.1
+            (dsL2.T_m.isel(time=it) - Ts) * 1000,
+            Zm.isel(time=it),
+            c="b",
+            alpha=(it + 1) / (2 * nt),
+            marker="x",
         )
     ax[0].plot(
         (dsL2.T_m.sel(time=att, method="nearest") - Ts) * 1000,
         Zm.sel(time=att, method="nearest"),
         c="k",
+        marker="x",
     )
-    ax[0].set_ylim((-H0 / 5, 0))
+    ax[0].set_ylim((-H0, 0))
     ax[0].set_xlim((-25, 1))
     ax[0].set_xlabel(r"$\overline{T}-T_s$ (mK)")
-    ax[0].set_ylabel(r"\overline{z}")
+    ax[0].set_ylabel(r"$\overline{z}$")
 
     print("-> Um profiles")
     this_eta = ds.eta.sel(time=att, method="nearest").values
@@ -96,21 +155,28 @@ if True:
 
     ax[1].vlines(0, -H0, 0, color="gray", ls="-")
     for it in range(0, nt, skip):
-        ax[1].plot(dsL2.u_m.isel(time=it), Zm.isel(time=it), c="b", alpha=0.1)
+        ax[1].plot(
+            dsL2.u_m.isel(time=it),
+            Zm.isel(time=it),
+            c="b",
+            alpha=(it + 1) / (2 * nt),
+            marker="x",
+        )
     ax[1].plot(
         dsL2.u_m.sel(time=att, method="nearest"),
         Zm.sel(time=att, method="nearest"),
         c="k",
         label="t=%d Tp" % (att / Tp),
+        marker="x",
     )
     ax[1].plot(
         us_Kenyon, Zm.sel(time=att, method="nearest"), c="k", ls="--", label=r"$U_s$"
     )
     ax[1].vlines(0, -H0, 0, color="gray", ls="-")
     ax[1].set_xlim((-0.01, 0.2))
-    ax[1].set_ylim((-H0 / 5, 0))
+    ax[1].set_ylim((-H0, 0))
     ax[1].set_xlabel(r"$\overline{u}$ (m/s)")
-    ax[1].set_ylabel(r"\overline{z}")
+    ax[1].set_ylabel(r"$\overline{z}$")
 
     print("-> Tke profiles")
     ax[2].vlines(0, -H0, 0, color="gray", ls="-")
@@ -119,52 +185,91 @@ if True:
             dsL2["tke"].mean(dim=["x", "y"]).isel(time=it),
             Zm.isel(time=it),
             c="b",
-            alpha=0.1,
+            alpha=(it + 1) / (2 * nt),
+            marker="x",
         )
     ax[2].plot(
         dsL2["tke"].mean(dim=["x", "y"]).sel(time=att, method="nearest"),
         Zm.sel(time=att, method="nearest"),
         c="k",
+        marker="x",
     )
-    ax[2].set_ylim((-H0 / 5, 0))
+    ax[2].set_ylim((-H0, 0))
     ax[2].set_xlabel(r"$\overline{tke}$ (m2/s2)")
-    ax[2].set_ylabel(r"\overline{z}")
+    ax[2].set_ylabel(r"$\overline{z}$")
 
     fig.savefig("CSI_mean_profile_evolution.pdf")
 
 # TODO:
-# - plot Stokes drift on U current plot
-
+# -
 
 # --- spectrum evolution ---
 if True:
-    start = att - 20
-    end = att + 20
+    print("Plotting spectrum !")
 
-    # find items in [start, end]
-    subds = ds.sel(time=slice(start, end))
+    listt = [0, 2 * Tp, 5 * Tp, 10 * Tp]
+    start = att - 5 * Tp
+    end = att + 5 * Tp
 
-    _, _, _, k_sample = get_wavenumber(N, L0 / N)
+    def compute_spectrum_trange(ds, start, end):
 
-    phi_k = np.zeros((len(subds.time), len(k_sample)))
-    kr = np.zeros((len(subds.time), len(k_sample)))
+        # find items in [start, end]
+        subds = ds.sel(time=slice(start, end))
+
+        _, _, _, k_sample = get_wavenumber(N, L0 / N)
+
+        phi_k = np.zeros((len(subds.time), len(k_sample)))
+        kr = np.zeros((len(subds.time), len(k_sample)))
+
+        for it in range(0, len(subds.time)):
+            this_eta = subds.eta.isel(time=it).values
+            tmp = get_spec_1D(this_eta, this_eta, L0 / N, averaging="radial")
+            phi_k[it] = tmp[1]
+            kr[it] = tmp[0]
+
+        kr *= 2 * np.pi
+
+        phi_k = phi_k.mean(axis=0)
+        kr = kr.mean(axis=0)
+        return kr, phi_k
 
     fig, ax = plt.subplots(1, 1, figsize=(5, 5), constrained_layout=True, dpi=100)
-    for it in range(0, len(subds.time)):
-        this_eta = subds.eta.isel(time=it).values
-        tmp = get_spec_1D(this_eta, this_eta, L0 / N, averaging="radial")
-        phi_k[it] = tmp[1]
-        kr[it] = tmp[0]
 
-    kr *= 2 * np.pi
+    for it in range(len(listt)):
+        this_eta = ds.eta.sel(time=listt[it], method="nearest")
+        kr, phi_k = get_spec_1D(this_eta, this_eta, L0 / N, averaging="radial")
 
-    phi_k = phi_k.mean(axis=0)
-    kr = kr.mean(axis=0)
-    ax.loglog(kr * L0, (phi_k / (2 * np.pi)) * kp**3, c=colors[it])
+        ax.loglog(
+            (kr * 2 * np.pi) * L0,
+            (phi_k / (2 * np.pi)) * kp**3,
+            c="b",
+            alpha=(it + 1) / (2 * len(listt)),
+            label="t/Tp=%d" % (listt[it] / Tp),
+        )
+
+    kr, phi_k = compute_spectrum_trange(ds, start, end)
+    ax.loglog(
+        kr * L0,
+        (phi_k / (2 * np.pi)) * kp**3,
+        c="k",
+        label="t/Tp=[%d,%d]" % (start / Tp, end / Tp),
+    )
+
+    # nice slope
+    x0, x1 = 1e2, 3e2  # adjust to sit in an empty-ish part of the plot
+    slope = -2.5
+    y0 = 1e-3  # adjust vertically so the line sits near your data
+    y1 = y0 * (x1 / x0) ** slope
+    ax.loglog([x0, x1], [y0, y1], "k--", lw=1)
+    ax.text(x1 * 1.1, y1, r"$k^{-2.5}$", fontsize=10, va="center")
+
     ax.set_xlabel("kL0")
     ax.grid()
+    ax.legend()
+    ax.set_xlim([10, 1000])
+    ax.set_ylim([1e-7, 1e-2])
     ax.set_ylabel(r"$\phi(k) k_p^{3}$")
-    fig.savefig("CSI_spectrum_evolution_t%d-%d.pdf" % (start, end))
+    fig.savefig("CSI_spectrum_t%d-%d.pdf" % (start, end))
 
 if False:
     fig, ax = plt.subplots(1, 1, figsize=(5, 5), constrained_layout=True, dpi=100)
@@ -219,6 +324,32 @@ if False:
 
 # -- Diag de MLD ---
 # TODO: !
+if False:
+    skip = 10
+
+    fig, ax = plt.subplots(1, 1, figsize=(5, 5), constrained_layout=True, dpi=100)
+    print("-> MLD diag: dTm/dz")
+    for it in range(0, nt, skip):
+        dTmdz = grad_dir(
+            dsL2.T_m.isel(time=it), dsL2.isel(time=it), grid, dir="Z", zvar="level"
+        )
+        ax.plot(
+            dTmdz,
+            Zm.isel(time=it),
+            c="b",
+            alpha=(it + 1) / (2 * nt),
+            marker="x",
+        )
+    # ax[0].plot(
+    #     (dsL2.T_m.sel(time=att, method="nearest") - Ts) * 1000,
+    #     Zm.sel(time=att, method="nearest"),
+    #     c="k",
+    #     marker="x",
+    # )
+    ax.set_ylim((-H0 / 5, 0))
+    # ax[0].set_xlim((-25, 1))
+    ax.set_xlabel(r"$\partial_z \overline{T}$")
+    ax.set_ylabel(r"$\overline{z}$")
 
 # ===========
 # 3D PLOTS
@@ -230,7 +361,7 @@ if False:
     render_snapshot(
         inpath + infile,
         "CSI_1rst_page",
-        ttime=att,
+        ttime=int(att),
         method="nearest",
         L0=L0,
         H0=H0,
